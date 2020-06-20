8231b0f30ca05146c018db5ca2860f993220321f
ShvedAn <shved@capyba.ru>
2020-06-20 12:04:51 +0400

    Пустой проект.

---
+++ .access.php
@@ -0,0 +1,7 @@
+<?
+$PERM["pub"]["5"]="T_8";
+$PERM["upload"]["5"]="T_8";
+$PERM["online"]["*"]="R";
+$PERM["desktop_app"]["*"]="R";
+$PERM["/"]["*"]="R";
+?>
---
+++ index.php
@@ -0,0 +1,7 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetPageProperty("title", "gazprom2020");
+$APPLICATION->SetTitle("Главная страница");
+?>
+
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
