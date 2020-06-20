c2e8ca0e2738dbee03047c90147575ea2b1e0a79
ShvedAn <shved@capyba.ru>
2020-06-20 18:36:49 +0400

    Правка стилей.

--- index.php
+++ index.php
@@ -3,5 +3,5 @@ require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetPageProperty("title", "gazprom2020");
 $APPLICATION->SetTitle("Главная страница");
 ?>
-
+222
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
